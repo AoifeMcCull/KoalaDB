35fc49064415d058be7787ee50c07fa31d696b62
Tom Pinataro <tompinataro@gmail.com>
2024-09-19 13:19:16 -0500

    Co-authored-by: Jamie McCullough <AoifeMcCull@users.noreply.github.com> Co-authored-by: BrockBiever <BrockBiever@users.noreply.github.com>

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,154 @@
 console.log( 'js' );
+getKoalas();
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
-  // axios call to server to get koalas
-  
-} // end getKoalas
 
-function saveKoala(){
-  console.log( 'in saveKoala' );
-  // axios call to server to get koalas
- 
+
+function getKoalas() {
+
+  axios({
+    method: 'GET',
+    url: '/koalas'
+  })
+
+  .then((response) => {
+    let koalaData = response.data;
+    console.log('This is the koala data we GET from server:', koalaData);
+
+    displayKoalas(koalaData);
+  })
+
+  .catch((err) => {
+    console.log('Error in getting data!', err);
+  })
+
+} // end of get
+
+
+
+// HTML
+
+function displayKoalas(data) {
+  console.log('This is the data to be displayed. Is this correct?:', data);
+  let viewKoalas = document.getElementById('viewKoalas');
+
+  viewKoalas.innerHTML = '';
+  for(let koala of data) {
+
+    console.log(`Is ${koala.name} ready to transfer? ${koala.ready_to_transfer}`);
+    if(koala.ready_to_transfer === true) {
+      viewKoalas.innerHTML += `
+
+      <tr>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.favorite_color}</td>
+        <td>${koala.ready_to_transfer}</td>
+        <td>${koala.notes}</td>
+        <td></td>
+        <td><button onClick="deleteKoala(${koala.id})">Delete</button></td>
+      </tr>`;
+    
+    } else {
+      viewKoalas.innerHTML += `
+
+      <tr>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.favorite_color}</td>
+        <td>${koala.ready_to_transfer}</td>
+        <td>${koala.notes}</td>
+        <td><button onClick="readyKoala(${koala.id})">Ready for Transfer</button></td>
+        <td><button onClick="deleteKoala(${koala.id})">Delete</button></td>
+      </tr>`;
+
+    } 
+    
+  } 
+} 
+
+
+// DELETE 
+
+function deleteKoala(koalaId) {
+  console.log('Koala id in client:', koalaId);
+
+  axios({
+    method: 'DELETE',
+    url: `/koalas/${koalaId}`
+  })
+
+  .then((response) => {
+    console.log(`${koalaId} now deleted:`, response);
+
+    getKoalas();
+  })
+
+
+  .catch((err) => {
+    console.log(`${koalaId} did not get deleted:`, err);
+  })
 }
 
-getKoalas();
+
+
+// Transfer Update
+function readyKoala(koala_id) {
+  axios ({
+    method: 'PUT',
+    url: `/koalas/${koala_id}`,
+    data: {status: 'true'}
+  })
+  .then((response) => {
+    getKoalas();
+  })
+  .catch((error) => {
+    console.log('error updating Transfer status',error)
+  })
+}
+
+
+
+
+
+function addKoala (event){
+  event.preventDefault();
+  console.log("Submit Button Clicked");
+  let name = document.getElementById('nameIn').value;
+  let age = document.getElementById('ageIn').value;
+  let color = document.getElementById('colorIn').value;
+  let readyForTransfer = document.getElementById('readyForTransferIn').value;
+  let notes = document.getElementById('notesIn').value;
+  console.log("Variables Match:", name, age, color, readyForTransfer,notes);
+
+let incomingKoalas = {
+  name: name,
+  favorite_color: color,
+  age: age,
+  ready_to_transfer:readyForTransfer,
+  notes: notes
+}
+
+  
+
+  console.log("incomingObject:", incomingKoalas);
+
+  
+
+  axios({
+    method: 'POST',
+    url: '/koalas',
+    data: incomingKoalas
+  }).then(function(response) {
+    console.log(response.data);
+    document.getElementById('form').reset();
+
+  }).catch(function(error) {
+    console.log('error in KoalasPOST', error); 
+    alert('Error adding koala object. Please try again later.')       
+  });
+
+
+}
+
+
+
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,17 +1,62 @@
 const express = require('express');
 const koalaRouter = express.Router();
-
 // DB CONNECTION
 
-
+const pool = require('../modules/pool');
 // GET
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "koala_info";';
+    pool.query(queryText).then(result => {
+    
+      res.send(result.rows);
+    }) 
+      .catch(error => {
+        console.log('error getting koala info', error);
+        res.sendStatus(500);
+      });
+  });
 
 
 // POST
+router.post('/', (req, res) => {
+    let newKoala = req.body;
+    console.log(`Adding Koala`, newKoala);
+  
 
+    let queryText = `INSERT INTO "koala_info" ("name", "favorite_color","age","transfer", "notes")
+                     VALUES ($1, $2, $3, $4, $5);`;
+    pool.query(queryText, [newKoala.name, newKoala.favorite_color, newKoala.age, newKoala.transfer, newKoala.notes])
+      .then(result => {
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error adding new koala`, error);
+        res.sendStatus(500);
+      });
+  });
 
 // PUT
+router.put('/:id', (req, res) => {
 
+    let koalaId = req.params.id;
+    let transfer  = req.body.transfer;
+  
+    let queryText = `
+    UPDATE "koala_info" SET "transfer"= NOT "transfer"
+    WHERE "id"= $1;
+`
+    console.log("Change ready for transfer: ", koalaId, transfer)
+
+
+    pool.query(queryText, [koalaId])
+      .then((result) => {
+        res.sendStatus(204)
+      })
+      .catch((err) => {
+        console.log(`Error making query ${queryText}'`, err)
+        res.sendStatus(500)
+      })
+  })
 
 // DELETE
 
